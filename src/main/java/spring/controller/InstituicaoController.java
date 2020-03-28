0e3a058a43fa4b0a3fd568f8d2618470dd4eb044
PauloMtz <pramosmtz@gmail.com>
2020-03-28 20:50:17 -0300

    Implementação da edição e exclusão de instituição

--- src/main/java/spring/controller/InstituicaoController.java
+++ src/main/java/spring/controller/InstituicaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +20,10 @@ public class InstituicaoController {
 	@Autowired
 	private InstituicaoRepositorio repositorio;
 	
+	// -----------------------------------------------------------------------------
+	// 						** LISTAR
+	// -----------------------------------------------------------------------------
+	
 	@GetMapping("/listar")
 	public ModelAndView index() { // instituicao/lista é pasta/arquivo.html
 		ModelAndView model = new ModelAndView("instituicao/lista");
@@ -27,11 +32,15 @@ public class InstituicaoController {
 		return model;
 	}
 	
+	// -----------------------------------------------------------------------------
+	// 						** INSERIR
+	// -----------------------------------------------------------------------------
+	
 	// carrega a página de inserção
 	@GetMapping("/inserir")
 	public ModelAndView inserir() {
 		ModelAndView model = new ModelAndView("instituicao/inserir");
-		model.addObject("instituicao", new Instituicao());
+		model.addObject("inserirInstituicao", new Instituicao());
 		return model; // envia objeto instituicao para a view
 	}
 	
@@ -39,7 +48,39 @@ public class InstituicaoController {
 	@PostMapping("/inserir")
 	public String inserir(Instituicao instituicao) {
 		repositorio.save(instituicao);
-		// instituicoes/lista => controller/action
+		// instituicoes/listar => controller/action
+		return "redirect:/instituicoes/listar";
+	}
+	
+	// -----------------------------------------------------------------------------
+	// 						** EDITAR
+	// -----------------------------------------------------------------------------
+	
+	// carrega página para edição
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable Long id) {
+		Instituicao instituicao = repositorio.findById(id).orElse(null);
+		ModelAndView model = new ModelAndView("instituicao/editar"); // pasta/arquivo
+		model.addObject("editarInstituicao", instituicao);
+		return model;
+	}
+	
+	// salva os dados quando o formulário for submetido
+	@PostMapping("/editar")
+	public String editar(Instituicao instituicao) {
+		repositorio.save(instituicao);
+		// instituicoes/listar => controller/action
+		return "redirect:/instituicoes/listar";
+	}
+	
+	// -----------------------------------------------------------------------------
+	// 						** EXCLUIR
+	// -----------------------------------------------------------------------------
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		repositorio.deleteById(id);
+		// instituicoes/listar => controller/action
 		return "redirect:/instituicoes/listar";
 	}
 }
