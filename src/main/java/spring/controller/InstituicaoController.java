c92de400e526fd80cd8945a1c6f7d27ce1e4874f
PauloMtz <pramosmtz@gmail.com>
2020-03-29 17:54:09 -0300

    Implementando pesquisa com Ajax

--- src/main/java/spring/controller/InstituicaoController.java
+++ src/main/java/spring/controller/InstituicaoController.java
@@ -1,6 +1,7 @@
 package spring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import spring.entidade.Instituicao;
@@ -82,5 +84,26 @@ public class InstituicaoController {
 		repositorio.deleteById(id);
 		// instituicoes/listar => controller/action
 		return "redirect:/instituicoes/listar";
+	}
+	
+	// -----------------------------------------------------------------------------
+	// 						** PESQUISA INSTITUIÇÃO POR NOME
+	// -----------------------------------------------------------------------------
+	
+	/*
+	 * o @PathVariable com o Optional<> informa que a String nome é opcional
+	 * o @ResponseBody é para retornar os dados em formato json
+	 * que será utilizado pelo Ajax
+	 */
+	
+	@GetMapping({"/pesquisar_instituicao/{nome}", "/pesquisar_instituicao"})
+	public @ResponseBody List<Instituicao> pesquisarInstituicao(@PathVariable Optional<String> nome) {
+		
+		// verifica se há um nome a ser pesquisado
+		if(nome.isPresent()) {
+			return repositorio.findByNomeContaining(nome.get());
+		} else {
+			return repositorio.findAll();
+		}
 	}
 }
--- src/main/java/spring/repositorio/InstituicaoRepositorio.java
+++ src/main/java/spring/repositorio/InstituicaoRepositorio.java
@@ -1,9 +1,14 @@
 package spring.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import spring.entidade.Instituicao;
 
 public interface InstituicaoRepositorio extends JpaRepository<Instituicao, Long> {
 
+	// pesquisa utilizando 'like' --> findByAtributoContaining(tipo atributo)
+	// o Spring Data fará a implementação do método de pesquisa
+	List<Instituicao> findByNomeContaining(String nome);
 }
